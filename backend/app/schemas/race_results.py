726a799ef9f5d1c93210780c7542890c70553ff6
IsaiasRVH2 <isaias.valdivia@uaz.edu.mx>
2026-03-09 21:55:13 -0600

    fix(backend): Changed the max value for the sprint and race pos in RaceResultsBase from 20 to 22

--- backend/app/schemas/race_results.py
+++ backend/app/schemas/race_results.py
@@ -4,9 +4,9 @@ from typing import Optional
 class RaceResultBase(BaseModel):
     gp_id: int
     driver_id: int
-    race_pos: int = Field(..., ge=1, le=20)
+    race_pos: int = Field(..., ge=1, le=22)
     race_points: int = Field(0, ge=0)
-    sprint_pos: Optional[int] = Field(None, ge=1, le=20)
+    sprint_pos: Optional[int] = Field(None, ge=1, le=22)
     sprint_points: int = Field(0, ge=0)
     gp_points: int = Field(0, ge=0)
     is_dnf: bool = False
